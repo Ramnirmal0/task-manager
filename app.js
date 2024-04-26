bb8959027188c9a0f03398d8a765ff3011b8b0dc
Nirmal Ram <nirmal@mozark.ai>
2024-04-26 19:31:13 +0530

    feature inititalised

--- app.js
+++ app.js
@@ -1,17 +1,85 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(port, (err) => {
-    if (err) {
-        return console.log('Something bad happened', err);
-    }
-    console.log(`Server is listening on ${port}`);
+app.get("/tasks", (req, res) => {
+  let result;
+  try {
+  } catch (error) {
+    result = {
+      statusCode: 405,
+      body: {
+        message: error,
+      },
+    };
+  }
+  res.send(result);
 });
 
+app.get("/tasks/:id", (req, res) => {
+  let result;
+  try {
+  } catch (error) {
+    result = {
+      statusCode: 405,
+      body: {
+        message: error,
+      },
+    };
+  }
+  res.send(result);
+});
 
+app.post("/tasks", (req, res) => {
+  let result;
+  try {
+  } catch (error) {
+    result = {
+      statusCode: 405,
+      body: {
+        message: error,
+      },
+    };
+  }
+  res.send(result);
+});
+
+app.put("/tasks/:id", (req, res) => {
+  let result;
+  try {
+  } catch (error) {
+    result = {
+      statusCode: 405,
+      body: {
+        message: error,
+      },
+    };
+  }
+  res.send(result);
+});
+
+app.delete("/tasks/:id", (req, res) => {
+  let result;
+  try {
+  } catch (error) {
+    result = {
+      statusCode: 405,
+      body: {
+        message: error,
+      },
+    };
+  }
+  res.send(result);
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log("Something bad happened", err);
+  }
+  console.log(`Server is listening on ${port}`);
+});
 
 module.exports = app;
